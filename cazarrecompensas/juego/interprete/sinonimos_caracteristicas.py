c4148642026aed047523b81d4f61b3d4cf7d0bef
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2024-11-28 17:31:34 +0100

    refactor(interprete): :truck: move sinonimos_caracteristicas into interprete folder

---
+++ cazarrecompensas/juego/interprete/sinonimos_caracteristicas.py
@@ -0,0 +1,23 @@
+SINONIMOS_CLAVES = {
+    #keys
+    ("cabello", "cabellera", "melena", "vello") : "pelo",
+    ("calvo", "calvicie", "falta de pelo", "pelado", "calvorota") : "entradas",
+    ("iris", "pupila", "retina") : "ojos",
+    ("anteojos", "lentes") : "gafas",
+    ("gorro", "boina", "sombrerillo") : "sombrero",
+    ("perilla", "patillas") : "barba",
+    ("mostacho") : "bigote",
+    ("joyeria", "accesorios") : "pendientes",
+    ("genero", "apariencia") : "sexo",
+}
+
+SINONIMOS_VALORES = {
+    ("canas", "canoso", "gris") : "blanco",
+    ("naranja", "pelirrojo", "bermejo", "zanahorio") : "rojo",
+    ("rubio, dorado") : "amarillo",
+    ("oscuro, moreno") : "negro",
+    ("castaño, pardo") : "marron",
+    ("celeste", "azulado") : "azul",
+    ("masculino", "varón", "señor", "varon", "chico") : "hombre",
+    ("femenino", "fémina", "señora", "femina", "chica") : "mujer"
+}
