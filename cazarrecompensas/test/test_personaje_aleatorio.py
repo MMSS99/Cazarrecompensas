ec43b88a7865e142415eeca6ff7e5bb6b19e1061
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2024-11-28 15:54:34 +0100

    test(test): :bug: fixed broken import in test_personaje_aleatorio

--- cazarrecompensas/test/test_personaje_aleatorio.py
+++ cazarrecompensas/test/test_personaje_aleatorio.py
@@ -1,5 +1,5 @@
 import pytest
-from cazarrecompensas.juego.personajes import personajes_aleatorios
+from cazarrecompensas.juego.personajes_aleatorios import personajes_aleatorios
 
 def test_personajes_aleatorios():
     assert personajes_aleatorios() in ["Susan", "Claire", "David", "Anne", "Robert", "Anita", "Joe", "George", "Bill", "Alfred", "Max", "Tom", "Alex", "Sam", "Richard", "Paul", "Maria", "Frans", "Herman", "Bernard", "Philip", "Eric", "Charles", "Peter"]
