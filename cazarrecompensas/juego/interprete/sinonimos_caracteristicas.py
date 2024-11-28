910518697366d62f1a6530a3ac23b330c7c820ba
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2024-11-28 17:42:42 +0100

    feat(interprete): :sparkles: add more synonyms

--- cazarrecompensas/juego/interprete/sinonimos_caracteristicas.py
+++ cazarrecompensas/juego/interprete/sinonimos_caracteristicas.py
@@ -1,23 +1,23 @@
 SINONIMOS_CLAVES = {
     #keys
-    ("cabello", "cabellera", "melena", "vello") : "pelo",
-    ("calvo", "calvicie", "falta de pelo", "pelado", "calvorota") : "entradas",
-    ("iris", "pupila", "retina") : "ojos",
-    ("anteojos", "lentes") : "gafas",
-    ("gorro", "boina", "sombrerillo") : "sombrero",
-    ("perilla", "patillas") : "barba",
-    ("mostacho") : "bigote",
-    ("joyeria", "accesorios") : "pendientes",
-    ("genero", "apariencia") : "sexo",
+    ("cabello", "cabellera", "melena", "vello", "pelo") : "pelo",
+    ("calvo", "calvicie", "falta de pelo", "pelado", "calvorota", "entrada", "entradas") : "entradas",
+    ("iris", "pupila", "retina", "ojo", "ojos") : "ojos",
+    ("anteojos", "lentes", "gafa", "gafas") : "gafas",
+    ("gorro", "boina", "sombrerillo", "sombrero") : "sombrero",
+    ("perilla", "patillas", "barba") : "barba",
+    ("mostacho", "bigote") : "bigote",
+    ("joyeria", "accesorios", "pendiente", "pendientes") : "pendientes",
+    ("genero", "apariencia", "sexo") : "sexo",
 }
 
 SINONIMOS_VALORES = {
-    ("canas", "canoso", "gris") : "blanco",
-    ("naranja", "pelirrojo", "bermejo", "zanahorio") : "rojo",
-    ("rubio, dorado") : "amarillo",
-    ("oscuro, moreno") : "negro",
-    ("castaño, pardo") : "marron",
-    ("celeste", "azulado") : "azul",
-    ("masculino", "varón", "señor", "varon", "chico") : "hombre",
-    ("femenino", "fémina", "señora", "femina", "chica") : "mujer"
+    ("canas", "canoso", "gris", "blancos", "blanco") : "blanco",
+    ("naranja", "pelirrojo", "bermejo", "zanahorio", "rojos", "rojo") : "rojo",
+    ("rubio", "dorado", "amarillo", "amarillos") : "amarillo",
+    ("oscuro", "moreno", "negros", "negro") : "negro",
+    ("castaño", "pardo", "marrones", "marron") : "marron",
+    ("celeste", "azulado", "azules", "azul") : "azul",
+    ("masculino", "varón", "señor", "varon", "chico", "hombre") : "hombre",
+    ("femenino", "fémina", "señora", "femina", "chica", "mujer") : "mujer"
 }
