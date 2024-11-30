cbb05411b9d37ba86e05c9e21add69cfb3346d74
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2024-11-30 21:27:15 +0100

    feat(index): :sparkles: functioning menu in index

--- cazarrecompensas/cazarrecompensas.py
+++ cazarrecompensas/cazarrecompensas.py
@@ -1,87 +1,32 @@
 import reflex as rx 
-from .juego import personajes_aleatorios
-from .juego.comprobador_caracteristicas import comprobar_caracteristicas
+from .state_menu import State_menu as State
 from .areadepruebas.areapruebas import areapruebas
-class State(rx.State):
-    personaje=""
-    caracteristica = ""
-    caracteristica_visor = ""
-    def generar_personaje(self):
-        self.personaje = personajes_aleatorios()
-    def actualizar_caracteristica(self, caracteristica):
-        self.caracteristica = caracteristica
-    def actualizar_caracteristicavisor (self):
-        self.caracteristica_visor = comprobar_caracteristicas(self.personaje, self.caracteristica)
-    
-    
-def action_bar() -> rx.Component:
-    return rx.hstack(
-        rx.input(
-            value=State.caracteristica,
-            on_change=State.actualizar_caracteristica,
-            placeholder="Pregúntame sobre el personaje",
-            radius="small",
-            width="30%",
-        ),
-        rx.button(
-            "Comprobar",
-            on_click=State.actualizar_caracteristicavisor
-        ),
-        justify="center",
-        width="75%"
-    )
+
 def index():
     return rx.box(
         rx.vstack(
-            rx.cond(
-                State.personaje != "",
-                rx.image(
-                    src=f"pj{State.personaje}.jpg",
-                    height="300px",
-                    border="5px solid #555"
-                ),
-                rx.text(
-                    "Selecciona un personaje", font_size="2em"
-                ),
-            ),
-            rx.box(
-                rx.heading(
-                f"La respuesta a tu pregunta es... {State.caracteristica_visor}",
-                font_size="2em",
-                align="center"
-                ),
-
-                background="radial-gradient(circle, rgba(172,31,8,0.37923672887123594) 0%, rgba(255,0,66,0.3008053563222164) 100%)",
+            rx.button(
+                "🚧 Ir al área de pruebas 🚧",
+                on_click=rx.redirect("/areapruebas"),
+                background_image="url('pruebas_amarillonegro.jpeg')",
                 border_radius="20px",
-                width="40%",
                 margin="12px",
                 padding="12px",
+                opacity=0.5,
+                _hover={
+                    "opacity": 1,
+                },
+            ),
 
-            ),
-            
-            rx.button(
-                "Nuevo Personaje",
-                on_click=State.generar_personaje,
-                color_scheme="tomato"
-            ),
-        
-            action_bar(),
-            align="center",
-            direction="column",
-            
+            align="center"
         ),
 
-        rx.button(
-            "Ir al menú",
-            on_click=rx.redirect(
-                "/areapruebas"
-                ),
-        ),
+
         background="center/cover url('fondoprueba.jpg')",
         width="100%",
         height="100vh",
         align="center"
-    )
+        )
 
 
 
