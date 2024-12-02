becdf0b5d4a17561bde96c31faaed452dfc2f508
Desteveco <destevezcounago@gmail.com>
2024-12-02 14:36:07 +0100

    feat: Grid creation

--- cazarrecompensas/juego/quien_es_quien.py
+++ cazarrecompensas/juego/quien_es_quien.py
@@ -1,5 +1,44 @@
 import reflex as rx
+from .personajes import PERSONAJES
+from ..areadepruebas.areapruebas import action_bar
 
+
+@rx.page(on_load=State.get_player)
 def juego():
-    return rx.text("TRABAJA")
+    caras = PERSONAJES.keys()
+    return rx.vstack( 
+        
+        rx.box(
+            rx.grid(
+                *[
+                    rx.box(
+                        rx.image(src=f"../pj{cara}.jpg", width="100%", height="100%"),
+                    
+                    
+                    )
+                    for cara in caras
+                ],
+                columns="8",
+                gap="5px",
+                align="center",
+            ),
+            max_height="100%",
+            align="center",
+            max_width="100vh"
+
+        ),
+        action_bar(),
+        padding="10px",
+        display="flex",
+        align="center",
+        width="100%",
+        justify_content="center",
+        align_items="center",
+        background="center/cover url('../fondoprueba.jpg')",
+        height="100vh",
+
     
+    
+    )
+
+
