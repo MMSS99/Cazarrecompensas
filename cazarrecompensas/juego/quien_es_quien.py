56b74447b2cb6ce8f792ea744ab316a93a12679c
Desteveco <destevezcounago@gmail.com>
2024-12-04 11:09:30 +0100

    feat: Frontend grid styled

--- cazarrecompensas/juego/quien_es_quien.py
+++ cazarrecompensas/juego/quien_es_quien.py
@@ -1,45 +1,51 @@
 import reflex as rx
 from .state_juego import State_juego as State
-from .partida.personajes import PERSONAJES
 from ..areadepruebas.areapruebas import action_bar
 
+
 def juego():
-    return rx.vstack( 
-        
-        rx.box(
-            rx.grid(
-                rx.foreach(
-                    State.listapersonajes,
-                    lambda i:rx.box(
-                        rx.image(src=f"../pj{i}.jpg", width="100%", height="100%"),
-                    
-                    
+    return rx.box(        
+        rx.vstack( 
+            rx.box(
+                rx.grid(
+                    rx.foreach(
+                        State.listapersonajes,
+                        lambda i:rx.box(
+                            rx.image(src=f"../pj{i}.jpg", width="100%", height="100%"),
+                        ), 
                     ),
-                    
-                ),
                 columns="8",
                 gap="5px",
                 align="center",
-            ),
+                
+                ),
+            
+            rx.box(
+                rx.heading(
+                f"La respuesta a tu pregunta es... {State.caracteristica_visor}",
+                font_size="2em",
+                align="center"
+                ),
             max_height="100%",
             align="center",
             max_width="100vh"
+            ),
 
+        
+            action_bar(),
+            rx.text(State.personaje_a_adivinar),
+            on_mount=State.nuevapartida,
+            
+            width="60vw",
+            height="60vh",
+            margin="auto",
+            align_items="center",
+            
+            
+            ),
         ),
-        action_bar(),
-        rx.text(State.personaje_a_adivinar),
-        on_mount=State.nuevapartida,
-        padding="10px",
-        display="flex",
-        align="center",
-        width="100%",
-        justify_content="center",
-        align_items="center",
         background="center/cover url('../fondoprueba.jpg')",
         height="100vh",
 
-    
-    
     )
-
 
