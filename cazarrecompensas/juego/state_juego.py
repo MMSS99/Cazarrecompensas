55299f3958bbd921036dd9d24672149e30d310b4
Desteveco <destevezcounago@gmail.com>
2024-12-04 11:10:06 +0100

    feat: implement action bar

--- cazarrecompensas/juego/state_juego.py
+++ cazarrecompensas/juego/state_juego.py
@@ -1,10 +1,16 @@
 import reflex as rx
 import random
 from .partida.personajes_aleatorios import personajes_aleatorios
+from ..juego.partida.comprobador_caracteristicas import comprobar_caracteristicas
 
 class State_juego(rx.State):
     listapersonajes=[]
     personaje_a_adivinar=""
+    caracteristica_visor = ""
+    
+    
+    def actualizar_caracteristicavisor (self):
+        self.caracteristica_visor = comprobar_caracteristicas(self.personaje_a_adivinar, self.caracteristica)
     def nuevapartida(self):
         self.listapersonajes=personajes_aleatorios()
         self.personaje_a_adivinar=random.choice(personajes_aleatorios())
